d9e147969e3511b1839d98f7f3e6f8b22a282114
caballerocsc <cesarsolau@hotmail.com>
2019-10-01 23:00:12 -0500

    actualizacion

--- src/app/ordenes/ordenes.component.ts
+++ src/app/ordenes/ordenes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Orden } from '../Ordenes/Orden';
+import { Orden } from '../ordenes/Orden';
 
 @Component({
   selector: 'app-ordenes',
@@ -7,7 +7,7 @@ import { Orden } from '../Ordenes/Orden';
   styleUrls: ['./ordenes.component.scss']
 })
 export class OrdenesComponent implements OnInit {
-  Ordenes:Orden[] = [
+  ordenes:Orden[] = [
     { id:1, nombre:'Susana', direccion :"Mercolan", valorT: "Mujer", cantidad: 10, fechaSol: "2019-09-30", fechaApro: "2019-09-30", fechaCierre: "2019-09-30", estado: "activo"},
     { id:2, nombre:'cesar', direccion :"Mercolan", valorT: "Mujer", cantidad: 10, fechaSol: "2019-09-30", fechaApro: "2019-09-30", fechaCierre: "2019-09-30", estado: "activo"}
 ];
