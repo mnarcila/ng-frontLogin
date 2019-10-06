aa7e02b2bf38340217fa80b26cea7007e7fdb164
caballerocsc <cesarsolau@hotmail.com>
2019-10-06 18:46:55 -0500

    se agrega cliente rest para ordenes

---
+++ src/app/_restOrdenes/model/models.ts
@@ -0,0 +1,7 @@
+export * from './detalleOrden';
+export * from './orden';
+export * from './ordenRsType';
+export * from './parametrosDeSalidaType';
+export * from './patchDocument';
+export * from './patchRequest';
+export * from './statusType';
