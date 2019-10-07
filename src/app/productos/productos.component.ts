15457f28bcf96022597a2b4cce71bdcd99bf9d2d
Mario <mnarcila@gmail.com>
2019-10-06 23:06:55 -0500

    asd
    asd

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -3,11 +3,17 @@ import { AuthService } from '../auth.service';
 import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductosInner, Producto, ProductoService, ProductoRsType, StatusType } from "../_restProducto";
 import { Router, ActivatedRoute } from '@angular/router';
-
+import {MatDividerModule} from '@angular/material/divider';
 export interface Estados {
   value: string;
   viewValue: string;
 }
+export interface Categorias {
+  value: string;
+  viewValue: string;
+}
+declare var $: any;
+
 
 @Component({
   selector: 'app-productos',
@@ -20,8 +26,15 @@ export class ProductosComponent implements OnInit {
     { value: 'ACTIVO', viewValue: 'ACTIVO' },
     { value: 'INACTIVO', viewValue: 'INACTIVO' }
   ];
+  categorias: Categorias[] = [
+    { value: '1', viewValue: 'Celulares0' }, 
+    { value: '1', viewValue: 'Celulares1' }, 
+  ];
+
   renderCrear: boolean = false;
   renderConsulta: boolean = false;
+  renderEditar: boolean = false;
+  
 
   title = 'OMS-kallsonys';
   loading = false;
@@ -46,7 +59,15 @@ export class ProductosComponent implements OnInit {
       descProduct: ['', Validators.required],
       valorProduct: ['', Validators.required],
       cateProduct: ['', Validators.required],
-      estadoProduct: ['', Validators.required]
+      estadoProduct: ['', Validators.required],
+      eidproducto: ['eidproducto', Validators.required],
+      eproduct: ['', Validators.required],
+      enameProduct: ['', Validators.required],
+      edescProduct: ['', Validators.required],
+      evalorProduct: ['', Validators.required],
+      ecateProduct: ['', Validators.required],
+      eestadoProduct: ['', Validators.required]
+
     });
   }
   get f() { return this.angForm.controls; }
@@ -63,7 +84,40 @@ export class ProductosComponent implements OnInit {
     console.log("size: " + this.tablaProductos.length);
     console.log("0: " + this.tablaProductos[0].nombre);
   }
-
+  editarProducto(): void { 
+    let producto: Producto = {};
+      producto.idProducto =   this.angForm.controls.eidproducto.value;
+      producto.nombre =       this.angForm.controls.enameProduct.value;
+      producto.descripcion =  this.angForm.controls.edescProduct.value;
+      producto.valorBase =    this.angForm.controls.evalorProduct.value;
+      producto.idCategoria =  this.angForm.controls.ecateProduct.value ;
+      producto.estado =       this.angForm.controls.eestadoProduct.value;
+      console.log(producto);
+      this.productoApi.actualizarProductoPorId('1', '1',producto.idProducto, producto).subscribe(
+        value => setTimeout(() => {
+          const prd = value;
+          this.consultaEspecifica(producto.idProducto);
+          this.mostrarNotificacion('Actualización de Producto','se actualizo con exito','success');
+        }, 200),
+        error => {
+          this.mostrarNotificacion('Actualización de Producto','se presento un error, por favor notifique al administrador','danger');
+          console.error(JSON.stringify(error))
+        },
+        () => console.log('done')
+      );
+      this.renderCrear= false;
+      this.renderConsulta = true; 
+      this.renderEditar = false; 
+        
+  }
+  renderCrearDiv() : void{
+    this.renderCrear= true;
+    this.renderConsulta = false; 
+    this.renderEditar = false; 
+  }
+  /**
+   * Evento cuando se da click en crear este debe ir y crearlo en base de datos 
+   */
   crearProducto(): void {
 
     let producto: Producto = {};
@@ -71,38 +125,49 @@ export class ProductosComponent implements OnInit {
     producto.nombre = this.angForm.controls.nameProduct.value;
     producto.descripcion = this.angForm.controls.descProduct.value;
     producto.valorBase = this.angForm.controls.valorProduct.value;
-    producto.idCategoria = 1; // this.angForm.controls.cateProduct.value ;
+    producto.idCategoria = this.angForm.controls.cateProduct.value ;
     producto.estado = this.angForm.controls.estadoProduct.value;
     console.log(producto);
     this.productoApi.registrarProducto('1', '1', producto).subscribe(
       value => setTimeout(() => {
         const prd = value;
         this.consultaEspecifica(value.productos[0].idProducto);
+        this.mostrarNotificacion('Creación de Producto','Producto creado con exito','success');
       }, 200),
-      error => console.error(JSON.stringify(error)),
+      error =>{
+        this.mostrarNotificacion('Creación de Producto','se presento un error, por favor notifique al administrador','danger');
+        console.error(JSON.stringify(error))
+      } ,
       () => console.log('done')
     );
     this.renderCrear= false;
     this.renderConsulta = true; 
+    this.renderEditar = false; 
   }
 
-  renderCrearDiv(): void {
-    if (this.renderCrear) {
-      this.renderCrear = false;
-    } else {
-      this.renderCrear = true;
-    }
-    this.renderConsulta= false;
+  consultarCategoria(idCategoria : number): String {
+    console.log(idCategoria);
+    return this.categorias[idCategoria].viewValue;
   }
 
-  renderConsultar(): void {
-    if (this.renderConsulta) {
-      this.renderConsulta = false;
-    } else {
-      this.renderConsulta = true;
-    }
-
+  /**evento de boton cuando se selecciona el editar */
+  editar(productoz:ProductosInner): void {
+    console.log("prd: "+productoz.idcategoria); 
+    this.angForm.controls.eidproducto.setValue(productoz.idProducto);
+    this.angForm.controls.enameProduct.setValue(productoz.nombre);
+    this.angForm.controls.edescProduct.setValue(productoz.descripcion);
+    this.angForm.controls.evalorProduct.setValue(productoz.valorBase);
+    
+    this.angForm.controls.ecateProduct.setValue(productoz.idcategoria);
+    this.angForm.controls.eestadoProduct.setValue(productoz.estado);
+     
+    this.renderCrear = false ; 
+    this.renderConsulta =  false;
+    this.renderEditar =  true;
   }
+ 
+
+ 
 
 
 
@@ -127,6 +192,8 @@ export class ProductosComponent implements OnInit {
    */
   onClick(): void {
     this.renderConsulta=true; 
+    this.renderCrear = false; 
+    this.renderEditar = false; 
     this.tablaProductos = [];
     console.log(this.angForm.controls.product.value);
 
@@ -159,5 +226,32 @@ export class ProductosComponent implements OnInit {
     }
     this.createForm();
   }
+
+  mostrarNotificacion(pTitulo : String , pTexto : String, pTipo: String ){
+
+ 
+    // const type = ['','info','success','warning','danger'];
+
+    const color = Math.floor((Math.random() * 4) + 1);
+
+    $.notify({
+        icon: "notifications",
+        message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+
+    },{
+        type: pTipo,
+        timer: 2000,
+        placement: {
+            from: 'bottom',
+            align: 'center'
+        },
+        template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+          '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+          '<i class="material-icons" data-notify="icon">notifications</i> ' +
+          '<span data-notify="title">'+pTitulo+'</span> ' +
+          '<span data-notify="message">'+pTexto+'</span>' +
+        '</div>'
+    });
+}
 
 }
